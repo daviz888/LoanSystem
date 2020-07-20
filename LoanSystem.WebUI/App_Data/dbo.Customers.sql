0fad31e06b63851bc0761444663f759cce1489cc
Daffy Duck <ayos888@gmail.com>
2020-07-20 13:23:21 +0800

    add sql script

---
+++ LoanSystem.WebUI/App_Data/dbo.Customers.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Customers] (
+    [CustomerID] INT            NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    [FirstName]  NVARCHAR (50)  NOT NULL,
+    [LastName]   NVARCHAR (50)  NOT NULL,
+    [Address]    NVARCHAR (MAX) NOT NULL,
+    [City]       NVARCHAR (50)  NULL,
+    [State]      NVARCHAR (50)  NULL,
+    [ZIP]        INT            NULL,
+);
+
